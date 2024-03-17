cea07af2e14e698c39ded5551ab794e75e376c6c
ninfa151 <night24608@icloud.com>
2024-03-17 14:44:33 +0900

    first commit

---
+++ CommunicationApp/CommunicationApp/Communication/Protocol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationApp.Communication
+{
+    internal abstract class Protocol
+    {
+        internal abstract void prepareConnection();
+        internal abstract void makeConnection();
+        internal abstract void closeConnection();
+    }
+}
---
+++ CommunicationApp/CommunicationApp/Communication/TCPServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationApp.Communication
+{
+    internal class TCPServer : Protocol
+    {
+        private TcpListener _listener;
+        internal TCPServer(IPAddress ip, int port)
+        {
+            _listener = new TcpListener(ip, port);
+        }
+        internal override void prepareConnection()
+        {
+            throw new NotImplementedException();
+        }
+        internal override void makeConnection()
+        {
+            throw new NotImplementedException();
+        }
+        internal override void closeConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
